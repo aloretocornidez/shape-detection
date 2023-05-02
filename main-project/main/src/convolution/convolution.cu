277c1267f18e7bf911b0ce23b2b22bdc2a0e5b86
rfuentesgtz <rfuentesgtz@email.arizona.edu>
2023-05-02 09:41:51 -0700

    Added streaming implementation of convolution

--- main-project/main/src/convolution/convolution.cu
+++ main-project/main/src/convolution/convolution.cu
@@ -39,7 +39,6 @@ __global__ void naiveConvolution(unsigned char input[], unsigned char mask[], un
             }
         }
     }
-
     output[row * cols + col] = pixVal;
 }
 
@@ -176,9 +175,32 @@ __global__ void sharedConvolutionDivergence(unsigned char input[], unsigned char
     
 }
 
+#define CHANNELS 3 // we have 3 channels corresponding to RGB
+// The input image is encoded as unsigned characters [0, 255]
+__global__ void colorConvert(unsigned char * rgbImage,
+    unsigned char * grayImage,
+    int width, int height) {
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    if (x < width && y < height) {
+        // get 1D coordinate for the grayscale image
+        int grayOffset = y*width + x;
+        // one can think of the RGB image having
+        // CHANNEL times columns than the gray scale image
+        int rgbOffset = grayOffset*CHANNELS;
+        unsigned char b = rgbImage[rgbOffset ]; // red value for pixel
+        unsigned char g = rgbImage[rgbOffset + 1]; // green value for pixel
+        unsigned char r = rgbImage[rgbOffset + 2]; // blue value for pixel
+        // perform the rescaling and store it
+        // We multiply by floating point constants
+        grayImage[grayOffset] = 0.21f*r + 0.71f*g + 0.07f*b;
+    }
+}
 
-//MAIN FUNCTION
-int main(void){
+
+
+//GPU Convolution Validation
+void gpuConvolutionTest(void){
 
 //--------------------SERIAL STARTUP FIRST--------------------
     srand(time(NULL)); //Set up random values
@@ -212,6 +234,7 @@ int main(void){
     int kRowDisplace = kRows / 2;
 
     cudaEventRecord(start);
+    
     for(int i=0; i < 720; ++i)              // rows
     {
         for(int j=0; j < 1280; ++j)          // columns
@@ -307,8 +330,8 @@ int main(void){
 
     //Call the naive kernel
     cudaEventRecord(startNaive);
-    int blockSize = 32;
 
+    int blockSize = 32;
     dim3 blockDim(blockSize,blockSize), gridDim( 1 + (cols - 1) / blockSize,
                                              1 + (rows - 1) / blockSize);
     naiveConvolution<<<gridDim, blockDim>>>
@@ -336,6 +359,8 @@ int main(void){
 
     //CUDA CONSTANT MEM
     cudaMemcpyToSymbol(consMask, hostMask, maskVal * maskVal);
+
+    
     cudaEvent_t startConstant, stopConstant;
     cudaEventCreate(&startConstant);
     cudaEventCreate(&stopConstant);
@@ -437,4 +462,351 @@ int main(void){
     free(hostInput);
     free(hostMask);
     free(hostOutput);
+}
+
+__constant__ unsigned char consMask1[maskDimension*maskDimension];
+__constant__ unsigned char consMask2[maskDimension*maskDimension];
+__constant__ unsigned char consMask3[maskDimension*maskDimension];
+
+void sampleKernelSingle(){
+//----------TEST IMAGE GENERATION----------
+    //Create a 720x1280 image
+    //ASSUMED format is 3 channels BGR
+    //tested with 5x5 masks
+    int rows = 720;
+    int columns = 1280;
+    float milliseconds = 0;
+    //Channel is defined as 3 above
+    srand(time(NULL)); //Set up random values
+
+    unsigned char inputImage[720][1280][CHANNELS];
+    unsigned char* hostMask;
+    unsigned char mask1[5][5];
+    unsigned char mask2[5][5];
+    unsigned char mask3[5][5];
+
+    //Initialize Input Image
+    for(int i = 0; i < 720; i++){
+        for(int j = 0; j < 1280; j++){
+            for(int k = 0; k < CHANNELS; k++){
+                inputImage[i][j][k] = (char)rand();
+            }
+        }
+    }
+
+    //Initialize all three masks
+    for(int i = 0; i < 1280; i++){
+        for(int j = 0; j < CHANNELS; j++){
+            mask1[i][j] = (char)rand();
+        }
+    }
+
+    //Initialize all three masks
+    for(int i = 0; i < 1280; i++){
+        for(int j = 0; j < CHANNELS; j++){
+            mask2[i][j] = (char)rand();
+        }
+    }
+
+    //Initialize all three masks
+    for(int i = 0; i < 1280; i++){
+        for(int j = 0; j < CHANNELS; j++){
+            mask3[i][j] = (char)rand();
+        }
+    }
+
+
+//----------CUDA BEGIN----------
+    //Allocate memory for all arrays and intermediate results
+    //Memory Declarations and Allocations
+    unsigned char* hostBGRInput;
+    unsigned char* deviceBGRInput;
+    unsigned char* hostGrayImage;
+    unsigned char* deviceGrayImage;
+    unsigned char* hostConvolved1;
+    unsigned char* deviceConvolved1;
+    unsigned char* hostConvolved2;
+    unsigned char* deviceConvolved2;
+    unsigned char* hostConvolved3;
+    unsigned char* deviceConvolved3;
+
+    //Malloc Host
+    hostBGRInput = (unsigned char*)malloc(rows * columns * CHANNELS * sizeof(unsigned char));
+    hostGrayImage = (unsigned char*)malloc(rows * columns * sizeof(unsigned char));
+    hostConvolved1 = (unsigned char*)malloc(rows * columns * sizeof(unsigned char));
+    hostConvolved2 = (unsigned char*)malloc(rows * columns * sizeof(unsigned char));
+    hostConvolved3 = (unsigned char*)malloc(rows * columns  * sizeof(unsigned char));
+
+    //CudaMalloc
+    err = cudaMalloc((void**)&deviceBGRInput, rows * columns * CHANNELS * sizeof(unsigned char));
+    err = cudaMalloc((void**)&deviceGrayImage, rows * columns * sizeof(unsigned char));
+    err = cudaMalloc((void**)&deviceConvolved1, rows * columns * sizeof(unsigned char));
+    err = cudaMalloc((void**)&deviceConvolved2, rows * columns * sizeof(unsigned char));
+    err = cudaMalloc((void**)&deviceConvolved3, rows * columns * sizeof(unsigned char));
+
+    //Generate input array on Host
+    //Initialize Input Image
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            for(int k = 0; k < CHANNELS; k++){
+                hostBGRInput[i * columns * CHANNELS + j * CHANNELS + k] = inputImage[i][j][k];
+            }
+        }
+    }
+
+    //Dimensions for blocks and grayscale grid
+    int blockSize = 32;
+    int maskVal = 5;
+    int gridTileSize = 28;
+    dim3 blockDim(blockSize,blockSize), gridDim( 1 + (columns - 1) / blockSize,
+                                             1 + (rows - 1) / blockSize);
+    //Grid for convolution shared
+    dim3 sharedGridDim( 1 + (columns - 1) / gridTileSize,
+                1 + (rows - 1) / gridTileSize);
+
+    //Constant memory
+    hostMask = (unsigned char*)malloc(maskVal * maskVal * sizeof(unsigned char));
+    for(int i = 0; i < maskVal; i++){
+        for(int j = 0; j < maskVal; j++){
+            hostMask[i * maskVal + j] = mask1[i][j];
+        }
+    }
+    cudaMemcpyToSymbol(consMask, hostMask, maskVal * maskVal);
+
+    //BEGIN OPERATIONS
+    //Copy Input array to GPU
+    cudaEvent_t kernelLaunch, kernelEnd;
+    cudaEventCreate(&kernelLaunch);
+    cudaEventCreate(&kernelEnd);
+    cudaEventRecord(kernelLaunch);
+
+    err = cudaMemcpy(deviceBGRInput, hostBGRInput, rows * columns * CHANNELS * sizeof(unsigned char), cudaMemcpyHostToDevice);
+    
+    colorConvert<<<gridDim, blockDim>>>(deviceBGRInput, deviceGrayImage, columns, rows);
+
+    sharedConvolution<<<sharedGridDim, blockDim>>>
+        (deviceGrayImage, deviceConvolved1, rows, columns, maskVal);
+    sharedConvolution<<<sharedGridDim, blockDim>>>
+        (deviceConvolved1, deviceConvolved2, rows, columns, maskVal);
+    sharedConvolution<<<sharedGridDim, blockDim>>>
+        (deviceConvolved2, deviceConvolved3, rows, columns, maskVal);
+
+    err = cudaMemcpy(hostConvolved3, deviceConvolved3, rows * columns * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+    cudaEventRecord(kernelEnd);
+    cudaEventSynchronize(kernelEnd);
+    milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, kernelLaunch, kernelEnd);
+    printf("Total Single Kernel time: %fms\n", milliseconds);
+    printf("estimated time for 30 images: %fms\n", milliseconds * 30);
+
+
+
+
+
+    //FREE HOST
+    free(hostBGRInput);
+    free(hostGrayImage);
+    free(hostConvolved1);
+    free(hostConvolved2);
+    free(hostConvolved3);
+
+    //FREE DEVICE
+    cudaFree(deviceBGRInput);
+    cudaFree(deviceGrayImage);
+    cudaFree(deviceConvolved1);
+    cudaFree(deviceConvolved2);
+    cudaFree(deviceConvolved3);
+}
+
+//--------------------------------STREAMING TEST-----------------------------------------
+#define stream_size 2
+#define num_images 5
+void betaStreamTest(){
+    //----------TEST IMAGE GENERATION----------
+    //Create a 720x1280 image
+    //ASSUMED format is 3 channels BGR
+    //tested with 5x5 masks
+    int rows = 720;
+    int columns = 1280;
+    float milliseconds = 0;
+    //Channel is defined as 3 above
+    srand(time(NULL)); //Set up random values
+
+    unsigned char inputImage[720][1280][CHANNELS];
+    unsigned char* hostMask;
+    unsigned char mask1[5][5];
+    unsigned char mask2[5][5];
+    unsigned char mask3[5][5];
+
+    //Initialize Input Image
+    for(int i = 0; i < 720; i++){
+        for(int j = 0; j < 1280; j++){
+            for(int k = 0; k < CHANNELS; k++){
+                inputImage[i][j][k] = (char)rand();
+            }
+        }
+    }
+
+    //Initialize all three masks
+    for(int i = 0; i < 1280; i++){
+        for(int j = 0; j < CHANNELS; j++){
+            mask1[i][j] = (char)rand();
+        }
+    }
+
+    //Initialize all three masks
+    for(int i = 0; i < 1280; i++){
+        for(int j = 0; j < CHANNELS; j++){
+            mask2[i][j] = (char)rand();
+        }
+    }
+
+    //Initialize all three masks
+    for(int i = 0; i < 1280; i++){
+        for(int j = 0; j < CHANNELS; j++){
+            mask3[i][j] = (char)rand();
+        }
+    }
+
+
+//----------CUDA BEGIN----------
+    //Allocate memory for all arrays and intermediate results
+    //Memory Declarations and Allocations
+    unsigned char* hostBGRInput[num_images];
+    unsigned char* deviceBGRInput[num_images];
+    unsigned char* hostGrayImage[num_images];
+    unsigned char* deviceGrayImage[num_images];
+    unsigned char* hostConvolved1[num_images];
+    unsigned char* deviceConvolved1[num_images];
+    unsigned char* hostConvolved2[num_images];
+    unsigned char* deviceConvolved2[num_images];
+    unsigned char* hostConvolved3[num_images];
+    unsigned char* deviceConvolved3[num_images];
+
+    //Malloc Host
+    for (int i = 0; i < num_images; i++){
+        cudaHostAlloc( (void**)&hostBGRInput[i], rows * columns * CHANNELS * sizeof(unsigned char), cudaHostAllocDefault);
+        cudaHostAlloc( (void**)&hostGrayImage[i], rows * columns * sizeof(unsigned char), cudaHostAllocDefault);
+        cudaHostAlloc( (void**)&hostConvolved1[i], rows * columns * sizeof(unsigned char), cudaHostAllocDefault);
+        cudaHostAlloc( (void**)&hostConvolved2[i], rows * columns * sizeof(unsigned char), cudaHostAllocDefault);
+        cudaHostAlloc( (void**)&hostConvolved3[i], rows * columns * sizeof(unsigned char), cudaHostAllocDefault);
+
+        // hostBGRInput[i] = (unsigned char*)malloc(rows * columns * CHANNELS * sizeof(unsigned char));
+        // hostGrayImage[i] = (unsigned char*)malloc(rows * columns * sizeof(unsigned char));
+        // hostConvolved1[i] = (unsigned char*)malloc(rows * columns * sizeof(unsigned char));
+        // hostConvolved2[i] = (unsigned char*)malloc(rows * columns * sizeof(unsigned char));
+        // hostConvolved3[i] = (unsigned char*)malloc(rows * columns  * sizeof(unsigned char));
+    }
+
+    //CudaMalloc
+    for(int i = 0; i < num_images; i++){
+        err = cudaMalloc((void**)&deviceBGRInput[i], rows * columns * CHANNELS * sizeof(unsigned char));
+        err = cudaMalloc((void**)&deviceGrayImage[i], rows * columns * sizeof(unsigned char));
+        err = cudaMalloc((void**)&deviceConvolved1[i], rows * columns * sizeof(unsigned char));
+        err = cudaMalloc((void**)&deviceConvolved2[i], rows * columns * sizeof(unsigned char));
+        err = cudaMalloc((void**)&deviceConvolved3[i], rows * columns * sizeof(unsigned char));
+    }
+
+    //Generate input array on Host
+    //Initialize Input Image
+    for(int s = 0; s < num_images; s++){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                for(int k = 0; k < CHANNELS; k++){
+                    hostBGRInput[s][i * columns * CHANNELS + j * CHANNELS + k] = inputImage[i][j][k];
+                }
+            }
+        }
+    }
+    //Dimensions for blocks and grayscale grid
+    int blockSize = 32;
+    int maskVal = 5;
+    int gridTileSize = 28;
+    dim3 blockDim(blockSize,blockSize), gridDim( 1 + (columns - 1) / blockSize,
+                                             1 + (rows - 1) / blockSize);
+    //Grid for convolution shared
+    dim3 sharedGridDim( 1 + (columns - 1) / gridTileSize,
+                1 + (rows - 1) / gridTileSize);
+
+    //Constant memory
+    hostMask = (unsigned char*)malloc(maskVal * maskVal * sizeof(unsigned char));
+    for(int i = 0; i < maskVal; i++){
+        for(int j = 0; j < maskVal; j++){
+            hostMask[i * maskVal + j] = mask1[i][j];
+        }
+    }
+    cudaMemcpyToSymbol(consMask, hostMask, maskVal * maskVal);
+
+    //Initialize stream
+    cudaStream_t streams[stream_size];
+    for(int i = 0; i < stream_size; i++){
+        cudaStreamCreate(&streams[i]);
+    }
+
+    //BEGIN OPERATIONS
+    //Copy Input array to GPU
+    cudaEvent_t kernelLaunch, kernelEnd;
+    cudaEventCreate(&kernelLaunch);
+    cudaEventCreate(&kernelEnd);
+    cudaEventRecord(kernelLaunch);
+
+    for(int j = 0; j < num_images; j = j + stream_size){
+        for(int i = 0; i < stream_size; i++){
+            err = cudaMemcpyAsync(deviceBGRInput[i + j], hostBGRInput[i + j], rows * columns * CHANNELS * sizeof(unsigned char), cudaMemcpyHostToDevice, streams[i]);
+            
+            colorConvert<<<gridDim, blockDim, 0, streams[i]>>>(deviceBGRInput[i + j], deviceGrayImage[i + j], columns, rows);
+
+            sharedConvolution<<<sharedGridDim, blockDim, 0, streams[i]>>>
+                (deviceGrayImage[i + j], deviceConvolved1[i + j], rows, columns, maskVal);
+            sharedConvolution<<<sharedGridDim, blockDim, 0, streams[i]>>>
+                (deviceConvolved1[i + j], deviceConvolved2[i + j], rows, columns, maskVal);
+            sharedConvolution<<<sharedGridDim, blockDim, 0, streams[i]>>>
+                (deviceConvolved2[i + j], deviceConvolved3[i + j], rows, columns, maskVal);   
+
+            err = cudaMemcpyAsync(hostConvolved3[i + j], deviceConvolved3[i + j], rows * columns * sizeof(unsigned char), cudaMemcpyDeviceToHost, streams[i]);
+            cudaStreamSynchronize(streams[i]);
+        }
+    }
+    cudaDeviceSynchronize();
+    cudaEventRecord(kernelEnd);
+    cudaEventSynchronize(kernelEnd);
+    milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, kernelLaunch, kernelEnd);
+    printf("Total 30 Kernel time: %fms\n", milliseconds);
+    printf("estimated time for 30 images: %fms\n", milliseconds * 1);
+
+
+
+
+
+    //FREE HOST
+    // free(hostBGRInput);
+    // free(hostGrayImage);
+    // free(hostConvolved1);
+    // free(hostConvolved2);
+    // free(hostConvolved3);
+
+    //FREE DEVICE
+    for(int i = 0; i < num_images; i++){
+        cudaFreeHost(hostBGRInput[i]);
+        cudaFreeHost(hostGrayImage[i]);
+        cudaFreeHost(hostConvolved1[i]);
+        cudaFreeHost(hostConvolved2[i]);
+        cudaFreeHost(hostConvolved3[i]);
+        cudaFree(deviceBGRInput[i]);
+        cudaFree(deviceGrayImage[i]);
+        cudaFree(deviceConvolved1[i]);
+        cudaFree(deviceConvolved2[i]);
+        cudaFree(deviceConvolved3[i]);
+    }
+    
+}
+
+int main(void){
+    printf("Testing basic GPU convolution compatibility\n");
+    gpuConvolutionTest();
+    printf("\n\nTesting images with single stream element.\n");
+    sampleKernelSingle();
+    printf("\n\nTesting with 5 streams.\n");
+    betaStreamTest();
 }
